f494c41bf9a21f345c6aba708e24d1b731cf5d57
jpvalery <jp.valery@me.com>
2019-04-26 10:38:57 -0400

    Adding license info in /about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,9 +8,14 @@ const About = center => (
   <Layout>
     <Helmet title={'About BPP'} />
     <Header title="About BPP">A repository of the best pricing pages out there</Header>
-    <Container center={center}>
-      Discussing with colleagues, we felt we could use a <a href="https://reallygoodemails.com/" target="_blank">ReallyGoodEmails</a> but for pricing page.<br />
+    <Container>
+      Discussing with colleagues, we felt we could use a <a href="https://reallygoodemails.com/" target="_blank">ReallyGoodEmails</a> but for pricing page.<br /><br />
       So I decided to build it :). It's a work in progress and I'll update it based on feedback received and needs we'll identify.
+    </Container>
+    <Container>
+      <h3> License </h3>
+      The code of the website is released under the <a href="https://github.com/jpvalery/pricingpages/blob/master/LICENSE" target="_blank">MIT License</a>.<br />
+      The content of the website is released under the <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons BY-NC-SA 4.0 International License</a>.
     </Container>
   </Layout>
 );
