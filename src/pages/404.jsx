d961c80519f3d0ba35cc8f1363720d5b03082566
jpvalery <jp.valery@me.com>
2019-04-27 09:35:04 -0400

    🐛 Major tweaks for 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,26 +1,53 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Helmet from 'react-helmet';
+import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 import { Header } from 'components';
-import { Layout, Container } from 'layouts';
+import { Layout } from 'layouts';
+import errlogo from '../../static/404.png';
+
+const Wrapper = styled.section`
+  margin: auto;
+  text-align:center;
+  padding: 2rem 0 0 0;
+  width: 50%;
+  max-width: ${props => props.theme.layout[props.type]};
+  height: 100%;
+  flex: 1;
+  @media (max-width: ${props => props.theme.breakpoints.m}) {
+    width: 80%;
+  }
+  @media (max-width: ${props => props.theme.breakpoints.s}) {
+    width: 90%;
+  }
+  h4{
+    font-style: italic;
+    font-weight: 100 !important;
+  }
+  ul{
+    margin-left: 2rem;
+    margin-top: 0.6rem;
+  }
+`;
 
 const ErrorPage = center => (
   <Layout>
-    <Helmet title={'404'} />
-    <Header title="404" />
-    <Container center={center}>
-      <h1>Woops, something went wrong.</h1>
+  <Helmet title={'404 - BestPricingPages'} />
+  <Header title="Oops! 404">We messed up. Sorry about that.</Header>
+    <Wrapper>
+      <img src={errlogo} alt="BestPricingPages 404" />
       <h3>This page does not exist or is no longer reachable.</h3>
+      <h4>Or maybe never even existed in the first place...</h4>
+        <br />
+        <br />
       <h3>
-        You can return to the <Link to="/">Homepage</Link>.
+        ↪️ Return to the <Link to="/">Homepage</Link>.
       </h3>
-    </Container>
+        <br />
+        <br />
+    </Wrapper>
   </Layout>
 );
 
 export default ErrorPage;
-
-Container.propTypes = {
-  center: PropTypes.object,
-};
